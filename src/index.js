7c3c2dfe5a1cd9aa80a83b999fba9cc794d2fabd
Asbert Ma <stayinalive@msn.cn>
2019-05-08 10:48:57 +0800

    show apps function and static things

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './style/index.css'
 
-// import things from './things'
+import things from './things'
 import * as PX from 'pixi.js'
 import star from './createStar'
-// import spaceMan from './spaceMan'
-// import rocket from './rocket'
+import spaceMan from './spaceMan'
+import rocket from './rocket'
 import DApps from './apps'
+// import CreateText from './text'
 
+const canvasContainer = document.querySelector('#canvas')
+setFontSize()
+const step1 = document.querySelector('#step1')
+step1.classList.add('show')
 const app = new PX.Application({
   backgroundColor: '0x343B63',
   width: window.screen.width,
@@ -14,40 +19,67 @@ const app = new PX.Application({
 })
 app.stage.interactive = true
 
-document.body.appendChild(app.view)
+canvasContainer.insertBefore(app.view, step1)
 
-console.log(app.view.width, app.view.height)
+bindEvent()
 
 const t = star(app)
 // spaceMan(app)
-// rocket(app)
-t.speedHigh().then(() => {
-})
-
+rocket(app)
+t.speedHigh().then(() => {})
+// CreateText(app)
 // addStatic()
-addApps()
+// addApps()
 
-
-// function addStatic() {
-//   const items = addPics(things)
-// }
-
-async function addApps() {
-  const items = addPics(await DApps(app.view.width, app.view.height))
+function addStatic() {
+  const items = createPics(things)
+  items.forEach(item => {
+    app.stage.addChild(item)
+  })
 }
 
-function addPics (resources) {
+async function addApps() {
+  const items = createPics(await DApps(app.view.width, app.view.height))
+  items.forEach(item => {
+    item.interactive = true
+    item.buttonMode = true
+    item.alpha = 0.3
+    item.checked = false
+    item.anchor.x = 0.5
+    item.anchor.y = 0.5
+    item
+      .on('pointerover', function() {
+        if (!this.checked) {
+          this.alpha = 10
+        }
+      })
+      .on('pointerout', function() {
+        if (!this.checked) {
+          this.alpha = 0.3
+        }
+      })
+      .on('pointerdown', function() {
+        if (!this.checked) {
+          this.alpha = 1
+          this.scale.x *= 1.2
+          this.scale.y *= 1.2
+        }
+        this.checked = true
+      })
+    app.stage.addChild(item)
+  })
+}
+
+function createPics(resources) {
   let result = []
-  console.log(resources)
   resources.forEach(item => {
     const temp = new PX.Sprite(PX.Texture.fromImage(item.img, true))
     temp.scale.x = 0.07
     temp.scale.y = 0.07
-    temp.x =  item.x
-    temp.y =  item.y
-    temp.alpha = 0.3
-    app.stage.addChild(temp)
-    result.push(item)
+    temp.x = item.x
+    temp.y = item.y
+
+    result.push(temp)
   })
 
   return result
